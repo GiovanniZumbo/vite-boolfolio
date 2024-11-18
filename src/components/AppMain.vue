61af6d9fc0b69056115e8e9a14abb479f3a6833f
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-11-18 15:36:07 +0100

    configuration

---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getProjects() {
+
+        }
+    },
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>App Main</h1>
+        <h2>Content</h2>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
