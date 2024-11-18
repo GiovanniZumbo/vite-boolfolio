3c87ee22346e24ed90758a2ba654c7fcc62dcf5a
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-11-18 15:45:00 +0100

    api call through axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,13 +4,26 @@ import axios from 'axios';
 export default {
     data() {
         return {
-
+            projectList: [],
+            ApiURL: "http://127.0.0.1:8000/api/projects",
         }
     },
     methods: {
         getProjects() {
-
+            axios.get(this.ApiURL)
+                .then((response) => {
+                    // handle success
+                    console.log(response.data.results);
+                    this.projectList = response.data.results
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
         }
+    },
+    created() {
+        this.getProjects();
     },
 
 }
@@ -18,8 +31,20 @@ export default {
 
 <template>
     <div class="container">
-        <h1>App Main</h1>
-        <h2>Content</h2>
+        <h1 class="text-center my-3">My projects</h1>
+        <div class="row row-cols-3" v-for="project in projectList">
+            <div class="col">
+                <div class="card">
+                    <div class="card-title">
+                        title
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
