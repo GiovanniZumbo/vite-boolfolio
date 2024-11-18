b267c0ec9710d43ffb1b7cb81230944eb5ce3f6e
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-11-18 16:49:20 +0100

    created aboutme page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
 import 'bootstrap/scss/bootstrap.scss'
-import App from './App.vue'
+import App from '../App.vue'
 
 createApp(App).mount('#app')
---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="text-center my-3 ">About me</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae laudantium possimus numquam non, illo
+            provident, tempore similique commodi laborum mollitia delectus amet eum ipsam tempora doloremque et, porro
+            corrupti suscipit?</p>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
